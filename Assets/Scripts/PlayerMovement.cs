1b984f61afe71582fe9250265585f7829fe360d3
Nef <nef@LAPTOP-UD3S9838>
2023-07-11 12:13:33 -0600

    Added health system basis

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,32 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    
-    public Rigidbody2D rigidBody;
-    public float movementVelocity = 25f;
-    Vector2 movementInput;
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    #region Variables
+    private Rigidbody2D rigidBody;
+    [SerializeField]
+    private float movementSpeed = 25f;
+    private Vector2 movementInput;
+    #endregion
+
+    #region Constants
+    private const string AXIS_HORIZONTAL = "Horizontal";
+    private const string AXIS_VERTICAL = "Vertical";
+    #endregion
+
+    private void Start() {
+        this.rigidBody = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
-    {
+    private void Update() {
         //Get the input from X and Y axis
-        movementInput.x = Input.GetAxisRaw("Horizontal");
-        movementInput.y = Input.GetAxisRaw("Vertical");
+        movementInput.x = Input.GetAxisRaw(AXIS_HORIZONTAL);
+        movementInput.y = Input.GetAxisRaw(AXIS_VERTICAL);
     }
 
-    void FixedUpdate()
-    {
+    private void FixedUpdate() {
         //Pretty descriptive xd
         movementInput.Normalize();
 
         //Apply to the rigidbody the movement
-        rigidBody.velocity = movementInput * movementVelocity;
+        rigidBody.velocity = movementInput * movementSpeed;
     }
 }

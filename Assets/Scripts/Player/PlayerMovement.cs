73ba37d769d82aed6d45e6b18fd92a224f55d854
FernandoArevalo <prepa.2001@gmail.com>
2023-07-14 18:38:38 -0600

    Basic animation

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour {
 
 	#region Variables
 	private Rigidbody2D rigidBody;
+	private Animator animator;
 	[SerializeField]
 	private float movementSpeed = 25f;
 	private Vector2 movementInput;
@@ -19,13 +21,19 @@ public class PlayerMovement : MonoBehaviour {
 
 	private void Start() {
 		this.rigidBody = GetComponent<Rigidbody2D>();
+		this.animator = GetComponent<Animator>();
 	}
 
 	private void Update() {
 		//Get the input from X and Y axis
 		movementInput.x = Input.GetAxisRaw(AXIS_HORIZONTAL);
 		movementInput.y = Input.GetAxisRaw(AXIS_VERTICAL);
-	}
+
+		//Set the variables for the animations
+		animator.SetFloat("X speed", movementInput.x);
+        animator.SetFloat("Y speed", movementInput.y);
+        animator.SetFloat("Speed", movementInput.sqrMagnitude);
+    }
 
 	private void FixedUpdate() {
 		//Pretty descriptive xd
